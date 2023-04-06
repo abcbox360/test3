39887e94d8457039639e61db0807cfcf3de41a26
abcbox360 <122330116+abcbox360@users.noreply.github.com>
2023-04-06 09:22:22 +0800

    Initial commit

---
+++ images/linkwuxuxian.js
@@ -0,0 +1,22 @@
+﻿/*
+Copyright by Audi 2006
+http://audi.tw
+http://www.carousel.com.tw
+歡迎應用於無償用途散播，並請勿移除本版權宣告
+
+*/
+
+function clearLinkDot() {
+	var i, a, main;
+	for(i=0; (a = document.getElementsByTagName("a")[i]); i++) {
+		if(a.getAttribute("onFocus")==null) {
+			a.setAttribute("onFocus","this.blur();");
+		}else{
+			a.setAttribute("onFocus",a.getAttribute("onFocus")+";this.blur();");
+		}
+		a.setAttribute("hideFocus","hidefocus");
+	}
+}
+
+clearLinkDot();
+onload=clearLinkDot;
